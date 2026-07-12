517da9222ae834fe43709514795ef362e750889b
oscar francisco reyes guevara <oscar.reyes@est.ulsa.edu.ni>
2026-07-12 02:03:25 -0600

    refactor(.env fields): delete repeated fields prone to dsync

--- server/infrastructure/config/config.go
+++ server/infrastructure/config/config.go
@@ -18,11 +18,11 @@ func Load() *Config {
 
 	DatabaseURL := fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
 		os.Getenv("DB_SSLMODE"),
 	)
 
