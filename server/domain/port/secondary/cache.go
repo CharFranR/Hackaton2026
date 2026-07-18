f3f8bcbb864a5317d852da1fc40db094ff827e44
oscar francisco reyes guevara <oscar.reyes@est.ulsa.edu.ni>
2026-07-18 00:30:05 -0600

    align port with adapter, Remeber return (bool,error)

--- server/domain/port/secondary/cache.go
+++ server/domain/port/secondary/cache.go
@@ -9,5 +9,5 @@ type Cache interface {
 	Get(ctx context.Context, key string, dest any) (bool, error)
 	Set(ctx context.Context, key string, value any, ttl time.Duration) error
 	Delete(ctx context.Context, key string) error
-	Remember(ctx context.Context, key string, ttl time.Duration, dest any, loader func() error) error
+	Remember(ctx context.Context, key string, ttl time.Duration, dest any, loader func() error) (bool, error)
 }
